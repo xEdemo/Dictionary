779623b40bab95339d9ac58911f8c07f0a61251b
xEdemo <ianvrcek@gmail.com>
2024-05-22 09:40:24 -0400

    added canvacord

--- src/commands/dictonary/lookup.js
+++ src/commands/dictonary/lookup.js
@@ -3,8 +3,10 @@ const {
 	Client,
 	Interaction,
 	ApplicationCommandOptionType,
+	AttachmentBuilder,
 	PermissionFlagsBits,
 } = require("discord.js");
+const canvacord = require("canvacord");
 const { Infraction } = require("../../models");
 
 //interaction.channel.send()
@@ -17,23 +19,28 @@ module.exports = {
 	options: [
 		{
 			name: "user",
-			description: "Enter a user's global name with an @ to lookup their infractions.",
-			required: true,
+			description:
+				"Enter a user's global name with an @ to lookup their infractions.",
 			type: ApplicationCommandOptionType.Mentionable,
 		},
 	],
 
 	/**
-	 * 
-	 * @param {Client} client 
-	 * @param {Interaction} interaction 
+	 *
+	 * @param {Client} client
+	 * @param {Interaction} interaction
 	 */
 	callback: async (client, interaction) => {
-		const userId = interaction.options.get('user').value;
+		if (!interaction.inGuild()) {
+			interaction.reply("You can only run this command inside a server.");
+			return;
+		}
+
+		const mentionedUserId = interaction.options.get("user")?.value;
+		const userId = mentionedUserId || interaction.member.id;
+		const user = await interaction.guild.members.fetch(userId);
 
 		await interaction.deferReply();
-
-		const user = await interaction.guild.members.fetch(userId);
 
 		if (!user) {
 			await interaction.editReply(
@@ -49,19 +56,54 @@ module.exports = {
 			});
 
 			if (infractionRecord) {
-				let infractionDetails = `**User:** ${user.user.globalName || user.user.username}\n**Total Infractions:** ${infractionRecord.infractions}\n**Words Used:**\n`;
+				let allInfractions = await Infraction.find({ guildId: interaction.guild.id }).select('-_id infractions userId')
 
-				infractionRecord.wordUsed.forEach((word, index) => {
-					infractionDetails += ` ${index + 1}: ${word}\n`;
-				});
+				// Does not work
+				let currentRank = allInfractions.findIndex((infraction) => infraction.userId === userId) + 1;
 
-				await interaction.editReply(infractionDetails);
+				canvacord.Font.loadDefault();
+
+				const card = new canvacord.RankCardBuilder()
+					.setUsername(`${user.user.username} (Total Infractions: ${infractionRecord.infractions})`)
+					.setDisplayName(user.user.globalName)
+					// .setDiscriminator(user.user.discriminator)
+					.setAvatar(user.user.displayAvatarURL())
+					.setCurrentXP(infractionRecord.infractions % 10)
+					.setRequiredXP(10)
+					.setLevel(Math.floor(infractionRecord.infractions / 10))
+					.setRank(currentRank)
+					.setTextStyles({
+						level: `Infraction Threshold:`,
+						xp: `Infractions:`,
+						rank: `Rank:`,
+					});
+				// let infractionDetails = `**User:** ${
+				// 	user.user.globalName || user.user.username
+				// }\n**Total Infractions:** ${
+				// 	infractionRecord.infractions
+				// }\n**Words Used:**\n`;
+
+				// infractionRecord.wordUsed.forEach((word, index) => {
+				// 	infractionDetails += ` ${index + 1}: ${word}\n`;
+				// });
+
+				const data = await card.build({ format: 'png' });
+				const attachment = new AttachmentBuilder(data);
+
+				// await interaction.editReply(infractionDetails);
+				await interaction.editReply({ files: [attachment] });
 			} else {
-				await interaction.editReply(`User ${user.user.globalName || user.user.username} has no infractions.`)
+				await interaction.editReply(
+					`User ${
+						user.user.globalName || user.user.username
+					} has no infractions.`
+				);
 			}
-		} catch (error) {
+		} catch (err) {
 			console.log(`There was an error looking up this user: ${err}`);
-			await interaction.editReply(`There was an error looking up this user. Please try again later.`);
-		}	
+			await interaction.editReply(
+				`There was an error looking up this user. Please try again later.`
+			);
+		}
 	},
 };
